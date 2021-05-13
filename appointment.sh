7bf4d812dc4611f54b5270bbcabb68b5f033376c
khadkeaakash <khadke_aakash@yahoo.co.in>
2021-05-13 03:40:08 -0700

    Update appointment.sh

--- appointment.sh
+++ appointment.sh
@@ -1,4 +1,5 @@
 curl -vvv -k \
      -X GET "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=180001&date=14-05-2021" \
-     -H  "accept: application/json" \
-     -H  "Accept-Language: hi_IN"
+     -H "accept: application/json" \
+     -H "Authorization: Bearer abcd1234"
+     -H "Accept-Language: hi_IN"
